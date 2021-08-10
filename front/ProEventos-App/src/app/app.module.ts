86d37f9f72a35c5a744d369c6b67c6a22009f8b6
Leonardo Regioli <henrique_11@msn.com>
2021-08-10 15:57:47 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding.

--- front/ProEventos-App/src/app/app.module.ts
+++ front/ProEventos-App/src/app/app.module.ts
@@ -2,23 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PalestrantesComponent,
-    EventosComponent
+    EventosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IEventos } from 'src/models/interface/IEventos.models';
 
 @Component({
   selector: 'app-eventos',
@@ -8,20 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: IEventos[] = [];
+  public eventosFilter: IEventos[] = [];
+  public widthImg: number = 150;
+  public marginImg: number = 2;
+  public collapsedImg: boolean = false;
+  private _filterList: string = '';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
-    console.log(this.eventos);
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos')
+    this.http.get<IEventos[]>('http://localhost:5000/api/eventos')
       .subscribe(
-        resp => this.eventos = resp,
-        error => console.error(error)
+        resp => { this.eventos = resp; this.eventosFilter = resp; },
+        err => console.log(err)
       );
+  }
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(filter: string) {
+    this._filterList = filter;
+    this.eventosFilter = this.filterList ? this.filterEvent(this._filterList.toLocaleLowerCase()) : this.eventos;
+  }
+
+  private filterEvent(filterBy: string): IEventos[] {
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filterBy.toLowerCase()) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filterBy.toLowerCase()) !== -1)
   }
 }
